7cb583ba2e70bb153ae9e7d25d6a2d8398a562f6
Andrew-Neely-82 <andrew.neely82@gmail.com>
2023-01-25 09:18:40 -0600

    changed syntax to Caps

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,12 +1,12 @@
 USE codeup_test_db;
 
-select album_name as 'Albums' from albums;
-select album_name as 'Albums released before 1980' from albums where release_date < 1980;
-select album_name as 'Albums by Michael Jackson' from albums where artist_name = 'Michael Jackson';
+SELECT album_name AS 'Albums' FROM albums;
+SELECT album_name AS 'Albums released before 1980' FROM albums WHERE release_date < 1980;
+SELECT album_name AS 'Albums by Michael Jackson' FROM albums WHERE artist_name = 'Michael Jackson';
 
-update albums set sales = sales*10;
-select * from albums;
-update albums set release_date = 1800 where release_date < 1980;
-select * from albums where release_date < 1980;
-update albums set artist_name = 'Peter Jackson' where artist_name = 'Michael Jackson';
-select * from albums where artist_name = 'Peter Jackson';
+UPDATE albums set sales = sales*10;
+SELECT * FROM albums;
+UPDATE albums set release_date = 1800 WHERE release_date < 1980;
+SELECT * FROM albums WHERE release_date < 1980;
+UPDATE albums set artist_name = 'Peter Jackson' WHERE artist_name = 'Michael Jackson';
+SELECT * FROM albums WHERE artist_name = 'Peter Jackson';
