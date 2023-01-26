f02ad22d7208385b8e771d1353264900e5a97b75
Andrew-Neely-82 <andrew.neely82@gmail.com>
2023-01-26 11:48:30 -0600

    updated

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,42 +1,26 @@
-USE employees;
-
-# TODO 2: no issues
-SELECT first_name, last_name FROM employees
-WHERE first_name
-IN ('Irena','Vidya','Maya')
-ORDER BY first_name ASC;
-
-# TODO 3: No issues
-SELECT first_name, last_name FROM employees
-WHERE gender = 'm'
-  AND (first_name = 'Irena'
-    OR first_name = 'Vidya'
-    OR first_name = 'Maya')
-ORDER BY first_name ASC, last_name ASC;
-
-# TODO 4: No issues
-SELECT first_name, last_name FROM employees
-WHERE gender = 'm'
-  AND (first_name = 'Irena'
-    OR first_name = 'Vidya'
-    OR first_name = 'Maya')
-ORDER BY last_name ASC, first_name ASC;
-
-# TODO 5: no issues
 SELECT emp_no, last_name FROM employees
-WHERE last_name LIKE 'e%'
-   OR last_name LIKE '%e'
-ORDER BY emp_no ASC;
-
-# TODO 6: complete
-SELECT emp_no, last_name FROM employees
-WHERE last_name LIKE 'e%'
-   OR last_name LIKE '%e'
+WHERE last_name LIKE 'E%'
+AND last_name LIKE '%E'
 ORDER BY emp_no DESC;
 
-# SELECT * FROM employees
-# WHERE last_name LIKE 'e%e';
-#
-# SELECT * FROM employees
-# WHERE last_name LIKE '%q%'
-#   AND last_name NOT LIKE '%qu%';
+SELECT CONCAT(first_name,' ',last_name) as full_name FROM employees
+WHERE last_name LIKE 'E%'
+AND last_name LIKE '%E'
+ORDER BY emp_no DESC;
+
+SELECT * FROM employees
+WHERE birth_date = '1965-12-25';
+
+SELECT * FROM employees
+WHERE birth_date = '1965-12-25'
+AND hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+
+SELECT * FROM employees
+WHERE birth_date = '1965-12-25'
+AND hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+ORDER BY hire_date DESC, birth_date ASC;
+
+SELECT emp_no, CONCAT(first_name,' ',last_name) as full_name, DATEDIFF(NOW(),hire_date) as days_worked FROM employees
+WHERE birth_date = '1965-12-25'
+AND hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+ORDER BY hire_date DESC, birth_date ASC;
